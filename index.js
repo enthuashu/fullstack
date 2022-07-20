5b9bf261f28adcf56081bad4754ac95969efd289
Ashutosh Mishra <69295774+enthuashu@users.noreply.github.com>
2022-07-20 12:31:58 +0530

    query and params

--- index.js
+++ index.js
@@ -2,12 +2,60 @@ const express = require("express");
 const app = express();
 
 // writing first request for frontend // writing first API // writing an api
-app.get("/posts", (req, res) => {
-  res.json({ success: true, data: "All the posts data" });
+// api.get(route,callback)
+app.get("/feed", (req, res) => {
+  try {
+    res.json({ success: true, data: "All the feed data" });
+  } catch (error) {
+    res.json({ success: false });
+  }
 });
 
 app.get("/messages", (req, res) => {
-  res.json({ success: true, data: "All the messages" });
+  try {
+    res.json({ success: true, data: "All the messages" });
+  } catch (error) {
+    res.json({ success: false });
+  }
+});
+
+// api template
+app.get("route", (req, res) => {
+  try {
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false });
+  }
+});
+
+// profile information using params// : is used while writing api
+app.get("/profileinfo/:id", (req, res) => {
+  try {
+    let name = req.params.id; // this is the parameter passed at route
+
+    res.json({
+      success: true,
+      data: `Profile information of ${name} whose dob is ${req.query.dob}`,
+    });
+  } catch (error) {
+    res.status(404).json({ success: false });
+  }
+});
+
+//filtering using queries   // sent after writing ? and assigning variables. No extra thing on backend route
+app.get("/filters", (req, res) => {
+  try {
+    let page = req.query.page;
+    let brand = req.query.brand;
+    let price = req.query.price;
+
+    res.json({
+      success: true,
+      data: `Data of page number ${page} of ${brand} and price= ${price} `,
+    });
+  } catch (error) {
+    res.status(404).json({ success: false });
+  }
 });
 
 // writing atleast 5 requests and send their postman responses and code ss
